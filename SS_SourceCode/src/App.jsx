a8e6d20aa0852bbfa5ab83fbe0dc9b045504882c
Mayank Parmar <mayankparmar0708@gmail.com>
2024-11-14 23:46:36 +0530

    Added AdminLogin and DoctorReg. Pages

--- SS_SourceCode/src/App.jsx
+++ SS_SourceCode/src/App.jsx
@@ -4,6 +4,8 @@ import DoctorRequestPage from './pages/DoctorRequestPage/DoctorRequestPage';
 import ManagePage from './pages/ManagePage/ManagePage';
 import BottomBar from './pages/Patient_Navbar_and_bottombar/BottomBar';
 import Navbar from './pages/Patient_Navbar_and_bottombar/Navbar';
+import AdminLogin from './pages/AdminLoginPage/AdminLogin';
+import AddNewDoctor from './pages/DoctorRegistarionPage/AddNewDoctor';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,9 @@ function App() {
   return (
     <>
     {/* <DoctorRequestPage/> */}
-    <ManagePage/>
+    {/* <ManagePage/> */}
+    {/* <AdminLogin/> */}
+    <AddNewDoctor/>
     </>
   )
 }
