3dd0c164f1eab7658e5260ac4e516ba11d4f257c
Mayank Parmar <mayankparmar0708@gmail.com>
2024-11-17 10:07:53 +0530

    changes related to stylesheet

--- SS_SourceCode/src/pages/DoctorRegistarionPage/AddNewDoctor.jsx
+++ SS_SourceCode/src/pages/DoctorRegistarionPage/AddNewDoctor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './AddNewDoc.css'
+import './AddNewDoc.css';
 
 const AddNewDoctor = () => {
   const [firstName, setFirstName] = useState("");
@@ -28,62 +28,65 @@ const AddNewDoctor = () => {
   };
 
   return (
-    <section className="page">
-      <section className="container add-doctor-form">
-      
-        <h1 className="form-title">REGISTER A NEW DOCTOR</h1>
-        <br></br>
+    <section className="add-new-doctor-page">
+      <div className="add-new-doctor-container">
+        <h1 className="add-new-doctor-title">REGISTER A NEW DOCTOR</h1>
         <form>
-          <div className="first-wrapper">
-           
+          <div className="add-new-doctor-form-wrapper">
             <div>
               <input
                 type="text" placeholder="First Name"
                 value={firstName} onChange={(e) => setFirstName(e.target.value)}
+                className="add-new-doctor-input"
               />
               <input
                 type="text" placeholder="Last Name"
                 value={lastName} onChange={(e) => setLastName(e.target.value)}
+                className="add-new-doctor-input"
               />
               <input
                 type="email" placeholder="Email"
                 value={email} onChange={(e) => setEmail(e.target.value)}
+                className="add-new-doctor-input"
               />
               <input
                 type="tel" placeholder="Mobile Number"
                 value={phone} onChange={(e) => setPhone(e.target.value)}
+                className="add-new-doctor-input"
               />
-             
               <input
                 type="date" placeholder="Date of Birth"
                 value={dob} onChange={(e) => setDob(e.target.value)}
+                className="add-new-doctor-input"
               />
               <select
                 value={gender} onChange={(e) => setGender(e.target.value)}
+                className="add-new-doctor-select"
               >
                 <option value="">Select Gender</option>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
-                <option value="Female">Other</option>
-                
+                <option value="Other">Other</option>
               </select>
               <input
                 type="password" placeholder="Password"
                 value={password} onChange={(e) => setPassword(e.target.value)}
+                className="add-new-doctor-input"
               />
               <select
                 value={doctorDepartment} onChange={(e) => setDoctorDepartment(e.target.value)}
+                className="add-new-doctor-select"
               >
                 <option value="">Select Department</option>
                 {departmentsArray.map((dept, index) => (
                   <option value={dept} key={index}>{dept}</option>
                 ))}
               </select>
-              <button type="submit" className="register-btn">Register New Doctor</button>
+              <button type="submit" className="add-new-doctor-register-btn">Register New Doctor</button>
             </div>
           </div>
         </form>
-      </section>
+      </div>
     </section>
   );
 };
