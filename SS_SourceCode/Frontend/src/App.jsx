d4d65dcd6f57be7182ba6c6f170e21b8cd219140
Jeet Desai <jeetdesai2004@gmail.com>
2024-11-17 18:05:12 +0530

    reorganized the file structure

--- SS_SourceCode/Frontend/src/App.jsx
+++ SS_SourceCode/Frontend/src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import DoctorRequestPage from './pages/DoctorRequestPage/DoctorRequestPage';
-import ManagePage from './pages/ManagePage/ManagePage';
-import BottomBar from './pages/Patient_Navbar_and_bottombar/BottomBar';
-import Navbar from './pages/Patient_Navbar_and_bottombar/Navbar';
-import AdminLogin from './pages/AdminLoginPage/AdminLogin';
+
 import AddNewDoctor from './pages/DoctorRegistarionPage/AddNewDoctor';
 import AddMedicalRecord from './pages/AddMedicalRecord/AddMedicalRecord';
 import DoctorNavbar from './pages/DoctorDashboard/DoctorNavbar';
 import { BrowserRouter } from 'react-router-dom';
 import ViewAppointmentRequests from './pages/AppointmentRequestPage/ViewAppointmentRequests';
-import AppointmentDetails from './pages/AppointmentDetailsPage/AppointmentDetails';
-import HospitalInfoPage from './pages/HospitalInfopage/HospitalInfoPage';
 import DoctorDashboard from './pages/DoctorDashboard/DoctorDashboard';
 import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
-
 function App() {
   return (
     <>
@@ -31,8 +24,8 @@ function App() {
     {/* <AppointmentDetails/> */}
     {/* <ViewAppointmentRequestsContainer/> */}
     {/* <HospitalInfoPage/> */}
-    {/* <DoctorDashboard/> */}
-    <AdminDashboard/>
+    <DoctorDashboard/>
+    {/* <AdminDashboard/> */}
     </>
   )
 }
