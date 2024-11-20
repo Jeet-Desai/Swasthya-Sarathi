b60315e2ad09a3c68d34bd598030a7afb19b8a01
202201481 <202201481@daiict.ac.in>
2024-11-21 03:19:47 +0530

    fixed the spelling mistakes for the doc.js for import of doctor controller

--- SS_SourceCode/Backend/Routes/doc.js
+++ SS_SourceCode/Backend/Routes/doc.js
@@ -1,7 +1,7 @@
 import express from  'express';
 
 import { authenticate, restrict } from '../auth/verifyToken.js';
-import { updateAppointment } from '../Controllers/doctorController.js';
+import { updateAppointment } from '../Controllers/DoctorController.js';
 
 const router = express.Router();
 router.post('/upd_appo/:appointmentId',updateAppointment);
